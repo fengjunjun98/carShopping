b647d64af01215ecdaf854fcccb68053d68e3413
fengjunjun <fengjunjun@17of.com>
2015-01-24 09:26:33 +0800

    支付宝配置和签名MD5算法

---
+++ src/com/whlingwu/car/shopping/core/pay/alipay/alipayescow/sign/MD5.java
@@ -0,0 +1,81 @@
+/** * 文件名：MD5.java 
+ * * 版本信息： 
+ * 日期：2015-1-24 
+ * Copyright: Corporation 2015 
+ * 版权所有 * */
+package com.whlingwu.car.shopping.core.pay.alipay.alipayescow.sign;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/** 项目名称：carShopping 
+ * 类名称：MD5 
+* 功能：支付宝MD5签名处理核心文件，不需要修改
+* 版本：3.3
+* 修改日期：2012-08-17
+* 说明：
+* 以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
+* 该代码仅供学习和研究支付宝接口使用，只是提供一个
+ * 类描述：b2b,b2c，处理页面提交的请求。 
+ * 类描述：Bean,响应Action请求，对数据库进行操作，并把结果返回到Action。 
+ * Copyright: Copyright (c) 2015 by 芜湖领悟信息科技有限公司
+ * Company: b2c b2b game trading platform System 
+ * 创建人：凤军军
+ * 创建时间：2015-1-24 上午09:24:15 
+ * 修改人：凤军军 
+ * 修改时间：2015-1-24 上午09:24:15 
+ * 修改备注：
+ * @version 1.0* */
+
+public class MD5 {
+	/**
+     * 签名字符串
+     * @param text 需要签名的字符串
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static String sign(String text, String key, String input_charset) {
+    	text = text + key;
+        return DigestUtils.md5Hex(getContentBytes(text, input_charset));
+    }
+    
+    /**
+     * 签名字符串
+     * @param text 需要签名的字符串
+     * @param sign 签名结果
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static boolean verify(String text, String sign, String key, String input_charset) {
+    	text = text + key;
+    	String mysign = DigestUtils.md5Hex(getContentBytes(text, input_charset));
+    	if(mysign.equals(sign)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+
+    /**
+     * @param content
+     * @param charset
+     * @return
+     * @throws SignatureException
+     * @throws UnsupportedEncodingException 
+     */
+    private static byte[] getContentBytes(String content, String charset) {
+        if (charset == null || "".equals(charset)) {
+            return content.getBytes();
+        }
+        try {
+            return content.getBytes(charset);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
+        }
+    }
+
+}
